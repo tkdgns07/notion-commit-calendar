2aa70928b06b3b2d8dcd6c44111338bee763fa79
sanghun <sangia070122@gmail.com>
2024-11-01 19:42:02 +0900

    fixed build error(useRef)

--- src/app/setting/page.tsx
+++ src/app/setting/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import data from '@emoji-mart/data'
 import Picker from '@emoji-mart/react'
