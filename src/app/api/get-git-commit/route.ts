081d2700533fcabfeb56062953febdf42b66a74e
tkdgns07 <tkdgns070122@gmail.com>
2024-11-03 21:22:23 +0900

    test commmmmt

--- src/app/api/get-git-commit/route.ts
+++ src/app/api/get-git-commit/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from 'next/server';
 import axios from 'axios';
 import { PrismaClient } from "@prisma/client"
 
-//test용 주석 : ddddddddddddddd
+//test용 주석 : ddddddddddddddddd
 
 interface CommitFile {
   filename: string;
